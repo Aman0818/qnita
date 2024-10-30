7e66e54bd132d4267f4790b9d429787b58380be7
Aman Mishra <amanmishra.nitagartala@gmail.com>
2024-10-31 03:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,9 @@ def parrticipatingInsti():
     
 @app.after_request
 def add_no_cache(response):
-    if 'Cache-Control' not in response.headers and request.path.startswith('/static/'):
-        response.headers['Cache-Control'] = 'public, max-age=31536000'
-    else:
-        response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-        response.headers['Pragma'] = 'no-cache'
-        response.headers['Expires'] = '0'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
     return response
-
 if __name__ == '__main__':
     app.run(debug=False)
